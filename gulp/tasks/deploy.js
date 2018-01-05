adaadda97c1c468adda9d5572fa754f074c08867
Juan Di Nella <juan.dinella@gmail.com>
2018-01-05 14:35:44 -0300

    update deploy task

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,12 +1,11 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-var config = require('./config');
+var gulp = require('gulp')
+var ghPages = require('gulp-gh-pages')
 
 // This task only works on a branch name origin with master
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function () {
   return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
+    .pipe(ghPages())
+})
