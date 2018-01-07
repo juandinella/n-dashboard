fea92d7d7bb3b026939c11a92689ffda10df674f
Juan Di Nella <juan.dinella@gmail.com>
2018-01-07 18:22:29 -0300

    Update 2018-01-07T21:22:26.692Z

--- js/app.js
+++ js/app.js
@@ -1,33 +1,78 @@
-/* global jQuery, $ */
-
-$(document).ready(function () {
-  console.log('Penny')
-})
-
-/* global jQuery, $ */
-
-$(document).ready(function () {
-  console.log('Penny')
-})
-
 /* global $ */
 
-// $(document).ready(function () {
-//   $('#example').DataTable({
-//     bPaginate: false,
-//     bLengthChange: false,
-//     // bFilter: false,
-//     // bInfo: false,
-//     bSort: false,
-//     rowReorder: {
-//       update: true
-//     },
-//     columnDefs: [
-//       { targets: 0, visible: false }
-//     ],
-//     // scrollX: true
-//     // responsive: true
-//   })
-// })
+var ctx = $('#myChart')
+
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['1/12/17', '2/12/17', '3/12/17', '4/12/17', '5/12/17', '6/12/17', '7/12/17'],
+
+    datasets: [{
+      label: '',
+      data: [12, 19, 3, 5, 2, 3, 17],
+      backgroundColor: [
+        'rgba(254,213,0, 0.20)'
+      ],
+      borderColor: [
+        'rgba(254,213,0,1)'
+      ],
+      borderWidth: 4
+    }]
+  },
+  options: {
+    title: {
+      display: false
+    },
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize: 5
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(242,246,249,1)'
+        }
+
+      }],
+      xAxes: [{
+        gridLines: {
+          display: false
+        }
+      }]
+    }
+  }
+})
+/* global jQuery */
+
+;(function ($) {
+  var toggle = $('.statToggle')
+  var close = $('.close')
+  var modal = $('.modal')
+  var overlay = $('.overlay')
+
+  var closeModal = function () {
+    modal.removeClass('is-active')
+    overlay.removeClass('is-active')
+  }
+
+  toggle.on('click', function () {
+    modal.addClass('is-active')
+    overlay.addClass('is-active')
+  })
+
+  close.on('click', closeModal)
+  overlay.on('click', closeModal)
+
+  $(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+      modal.removeClass('is-active')
+      overlay.removeClass('is-active')
+    }
+  })
+})(jQuery)
 
 //# sourceMappingURL=app.js.map
