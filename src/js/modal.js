175847d271aa9d6ba87842b012fd984137fb3ef2
Juan Di Nella <juan.dinella@gmail.com>
2018-01-11 14:26:48 -0300

    update modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,27 +2,30 @@
 
 ;(function ($) {
   var toggle = $('.statToggle')
-  var close = $('.close')
   var modal = $('.modal')
   var overlay = $('.overlay')
 
-  var closeModal = function () {
+  function closeModal () {
     modal.removeClass('is-active')
     overlay.removeClass('is-active')
   }
 
   toggle.on('click', function () {
+    var nameID = ($(this).attr('id')).replace('toggle_', '')    // toggle_X
+    var modal = $('#modal_' + nameID)                           // modal_X
+    var modalClose = $('.close', modal)
+
     modal.addClass('is-active')
     overlay.addClass('is-active')
+
+    modalClose.on('click', closeModal)
   })
 
-  close.on('click', closeModal)
   overlay.on('click', closeModal)
 
   $(document).keydown(function (e) {
     if (e.keyCode === 27) {
-      modal.removeClass('is-active')
-      overlay.removeClass('is-active')
+      closeModal()
     }
   })
 })(jQuery)
